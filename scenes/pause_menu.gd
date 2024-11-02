ffd8f219bed5d790d00adf5869fff82471c54d5c
9retired <eyeballx7@gmail.com>
2024-11-02 16:02:04 -0700

    autoload pause menu
    rewrote pause menu to work on every scene.

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,15 +1,38 @@
-extends Control
+extends CanvasLayer
 
-signal on_continue
-signal on_restart
+func _ready():
+	visible = false
 
+#region pause menu buttons
 func _on_continue_button_pressed():
-	on_continue.emit();
-
-func _on_exit_button_pressed():
-	get_tree().quit();
+	unpause()
 
 #Sends the restart signal and the continue signal to unpause the game
 func _on_restart_button_pressed():
-	on_restart.emit();
-	on_continue.emit();
+	unpause()
+	get_tree().reload_current_scene()
+
+func _on_exit_button_pressed():
+	get_tree().quit();
+#endregion
+
+func _physics_process(delta):
+	# NOTE
+	# since autoload scripts can be referenced by all scripts in a scene.
+	# potentially change it so we just call toggle() instead of checking for inputs here.
+	if Input.is_action_just_pressed("pause_menu"):
+		toggle()
+
+func toggle():
+	if get_tree().paused == false:
+		pause()
+	else:
+		unpause()
+
+func pause():
+	visible = true
+	get_tree().paused = true
+
+func unpause():
+	visible = false
+	get_tree().paused = false
